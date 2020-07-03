a9ca1c07ca64534bffc13c6cf6eca9c1f85c9ab1
Gustavo Nemeth <hidan_nemeth@hotmail.com>
2020-07-03 16:28:32 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
-import csv
-import operator
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+import json
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
 
+img_width, img_height = 224, 224
 
-with open('table.csv', 'r') as file:
-    reader = csv.reader(file)
-    sortedlist = sorted(reader, key=operator.itemgetter(2), reverse=True)
-    print(sortedlist)
+train_data_dir = 'Train'
+validation_data_dir = 'Test'
+nb_train_samples = 4654
+nb_validation_samples = 1167
+epochs = 100
+batch_size = 16
+
+input_shape = (img_width, img_height, 3)
+
+model = Sequential()
+model.add(Conv2D(32, (7, 7), input_shape=input_shape)) 
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (5, 5)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(128, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
